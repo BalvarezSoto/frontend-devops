c63195ed43abd52a8343dbac54736d657d784b86
Eduardo-Guerra-Lobos <eguerra@usm.cl>
2026-06-19 12:18:47 -0400

    Generar reporte de coverage

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    }
   }
 })
